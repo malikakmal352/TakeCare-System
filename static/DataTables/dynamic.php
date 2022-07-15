18d32e566181a4ba73619ab61d5c3e44dcc9252c
Malik352 <malikakmalnaseer@gmail.com>
2022-07-15 21:26:10 +0500

    first Vision 1.0.1 Complete

--- static/DataTables/dynamic.php
+++
@@ -1,9 +0,0 @@
-window._site.forumUser = "";
-
-$(document).ready( function () {
-	window._site.dynamicLoaded();
-} );
-
-window._site.csrfToken = '6577f98b3889492806618f93e32c4ae1176abd77e25d346f';
-
-
